78edba2c784c18093ad1630d49abc9782c190e0c
Gabriel Caixeta Silva <gabriel@soft4.com.br>
2021-10-08 10:18:21 -0300

    teste fixing style

--- app/User.php
+++ app/User.php
@@ -36,4 +36,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $batata_doce = [
+        'email_verified_at' => 'datetime',
+    ];
 }

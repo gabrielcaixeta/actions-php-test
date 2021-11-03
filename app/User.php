0d0886867c0e22b9b29f10b72d9d2f004d7f7501
Gabriel Caixeta Silva <gabrielcaixeta@users.noreply.github.com>
2021-11-03 15:00:37 -0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -5,8 +5,7 @@ namespace App;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     use Notifiable;
 
     /**

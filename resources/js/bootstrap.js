da73b0ae2ef3a405c7c16d514b1f8e9cd95f1d61
Gabriel Caixeta Silva <gabriel@soft4.com.br>
2021-11-05 16:39:03 -0300

    aaaa

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,7 +10,7 @@ window.axios = require("axios");
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-let teste = "uytuytuyt";
+let teste = "uytuytfdsafdsafdsauyt";
 
 teste = "dsafdsafdsafdf666666666wefdsafdsafdas432";
 
